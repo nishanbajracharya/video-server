dfb42b1ecd91b507ee48d08b7c491bc4fef47cc4
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-02-15 23:26:34 +0545

    Read data from serve url and generate links

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
+
 import Nav from '../components/nav';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import * as videoService from '../services/video';
 
-    <Nav />
+videoService.getList();
 
-    <div>Home</div>
-  </div>
-);
+const Home = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    videoService.getList().then(list => setList(list));
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav />
+
+      <ul>
+        {list.map(video => (
+          <li key={video.id}>
+            <Link href={`/video/${video.id}`}>
+              <a>{video.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
---
+++ services/video.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const VIDEO_URL = process.env.SERVE_URL;
+
+export function getList() {
+  return axios(VIDEO_URL).then(response => {
+    const files = response.data.files;
+
+    return files.map((file, id) => ({
+      title: file.name,
+      id: 'video-' + id,
+      url: VIDEO_URL + '/' + file.base,
+    }));
+  });
+}
