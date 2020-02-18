befb56b7c22d3cd205d174958ec944bd4d4a65f0
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-02-18 13:08:06 +0545

    Add background

--- components/nav.js
+++ components/nav.js
@@ -17,7 +17,8 @@ const Nav = props => (
       />
       <style>
         {`body {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+          background: #fafafa;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
         }`}
       </style>
     </Head>
